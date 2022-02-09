797d4ef2584b727d32022d4da4783cc4c4c2de68
崔 孝俊 <2856801855@qq.com>
2022-02-09 10:55:13 +0800

    clouddebug

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -419,13 +419,56 @@ class WeGamePlayground {
         this.$playground.hide();
     }
 }
+class Settings {
+    constructor(root) {
+        this.root = root;
+        this.platform = "WEB";
+        if (this.root.AcWingOS) this.platform = "ACAPP";
+        this.start();
+    }
+    start(){
+        this.getinfo();
+    }
+    register() { //打开注册界面
+
+    }
+    login() { //打开登录界面
+
+    }
+    getinfo(){
+    let outer = this;
+        $.ajax({
+            url: "https://app1236.acapp.acwing.com.cn/settings/getinfo/",
+            type: "GET",
+            data:{
+                platform:outer.platform,
+            },
+            success: function(resp) {
+                console.log(resp);
+                if (resp.result === "success") {
+                    outer.hide();
+                    outer.root.menu.show();
+                } else {
+                    outer.login();
+                }
+            }
+        });
+    }
+    hide() {
+    }
+    show() {
+    }
+}
 export class WeGame {
-    constructor(id) {
+    constructor(id, AcWingOS) {
         this.id = id;
         this.$ac_game = $('#' + id);
+        
+        this.AcWingOS = AcWingOS;
+        
+        this.settings = new Settings(this);
         this.menu = new WeGameMenu(this);
         this.playground = new WeGamePlayground(this);
-
         this.start();
     }
 
--- static/js/src/zbase.js
+++ static/js/src/zbase.js
@@ -1,10 +1,13 @@
 export class WeGame {
-    constructor(id) {
+    constructor(id, AcWingOS) {
         this.id = id;
         this.$ac_game = $('#' + id);
+        
+        this.AcWingOS = AcWingOS;
+        
+        this.settings = new Settings(this);
         this.menu = new WeGameMenu(this);
         this.playground = new WeGamePlayground(this);
-
         this.start();
     }
 
